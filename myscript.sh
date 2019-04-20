b30c01ef9c37a333abda29d52cc480a33b6044c7
KhalifaGad <kkhalifa.gad@gmail.com>
2019-04-20 14:23:25 +0200

    yaaay! this the first script

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,14 @@
+#! /bin/bash
+
+# The first line is called hashbang or shebang, And its used to 
+# detrmine which interpreter used to interpret of the rest of file.
+
+echo the first param: $1, 'while 'the second $2
+echo
+
+echo No of params are : $#
+echo
+
+echo the whole params are: $@
+
 
